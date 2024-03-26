67052adf9ab0a051638b853dc6161cfaa1c9044d
filippogiovannoni <filippogiovannonii@gmail.com>
2024-03-26 11:33:13 +0100

    Initial commit add Class Product and Category

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+class Product
+{
+    public $name;
+    public $description;
+    public $price;
+    public $category;
+}
+
+class Category
+{
+    public $name;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Store</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+
+    <header class="bg-light">
+        <nav class="nav container justify-content-center p-3 gap-5">
+            <h2>The Pet Store</h2>
+            <a class="nav-link active" href="#" aria-current="page">Products</a>
+            <a class="nav-link" href="#">Categories</a>
+            <a class="nav-link disabled" href="#">Contact us</a>
+        </nav>
+    </header>
+
+    <main class="mt-3">
+        <div class="container">
+            <div class="row">
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+
+</body>
+
+</html>
