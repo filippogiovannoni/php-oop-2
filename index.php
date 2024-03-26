c382330dec60c854f506202748471a48ae411472
filippogiovannoni <filippogiovannonii@gmail.com>
2024-03-26 18:05:02 +0100

    add animal icon

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ require_once __DIR__ . '/database/db.php'
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet Store</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -40,7 +41,7 @@ require_once __DIR__ . '/database/db.php'
                                 <p><?= $product->description ?></p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <span class="badge bg-success"><?= $product->price . ' ' . '€' ?></span>
-                                    <span class="badge rounded-pill bg-info text-dark"><?= $product->category ?></span>
+                                    <i class="fa-solid fa-<?= strtolower($product->category) ?>"></i>
                                     <span class="badge rounded-pill bg-warning text-dark"><?= $product::class ?></span>
                                 </div>
                             </div>
